3dba9dae245b8fa0aef70258dd330e28557c9a46
prakash <satyap176@gmail.com>
2020-06-23 07:06:06 +0530

    first devcommit

---
+++ Dev_June_20-master/Lecture_2_JS_Scrapping_18_06_2020/activity/leaderboard/request.js
@@ -0,0 +1,79 @@
+// npm install request
+// to make a request to any server
+// node inbuilt module 
+let fs = require("fs");
+// request to any server=> using nodejs
+let request = require("request");
+// parse html
+let cheerio = require("cheerio");
+let eachMatchHandler = require("./getLeaderBoard");
+
+// console.log("Before");
+request("https://www.espncricinfo.com/series/_/id/8039/season/2015/icc-cricket-world-cup", dataReciever);
+// console.log("After");
+function dataReciever(err, res, html) {
+    // console.log("Inside dataReciver");/
+    if (err == null && res.statusCode == 200) {
+        //  console.log(res);
+        // console.log(html);
+        parsefile(html);
+    } else if (res.statusCode == 404) {
+        console.log("Page Not found");
+    } else {
+        console.log(err);
+        console.log(res);
+    }
+}
+// get url of all matches page
+function parsefile(html) {
+    let $ = cheerio.load(html);
+    // let list = $("ul.list-unstyled.mb-0");
+    // fs.writeFileSync("list.html",list);
+    let a = $("li.widget-items.cta-link a").attr("href");
+    console.log(a);
+    let fullLink = "https://www.espncricinfo.com" + a;
+    // console.log(fullLink);
+    // get html of all matches page
+    request(fullLink, matchPageHandler);
+}
+
+function matchPageHandler(err, res, html) {
+    if (err == null && res.statusCode == 200) {
+        //  console.log(res);
+        // console.log(html);
+        parseMatch(html);
+    } else if (res.statusCode == 404) {
+        console.log("Page Not found");
+    } else {
+        console.log(err);
+        console.log(res);
+    }
+}
+
+// get link of all the matches
+function parseMatch(html) {
+    let $ = cheerio.load(html);
+    // in full Page
+    let allCards = $(".col-md-8.col-16");
+    // console.log("allCard.html",allCards);
+    // fs.writeFileSync("allCard.html",allCards);
+    console.log("Before");
+    for (let i = 0; i < allCards.length; i++) {
+        // find => find in element 
+        // let result = $(allCards[i]).find(".extra-small.mb-0.match-description.match-description-bottom").text();
+        // let details = $(allCards[i]).find(".small.mb-0.match-description").text();
+        let allanchors = $(allCards[i]).find(".match-cta-container a")
+        let scoreCardLink = $(allanchors[0]).attr("href");
+        // console.log(result);
+        // console.log(details);
+        // console.log("https://www.espncricinfo.com" + scoreCardLink);
+        eachMatchHandler("https://www.espncricinfo.com" + scoreCardLink);
+        // let url = "https://www.espncricinfo.com" + scoreCardLink;
+        // use => match.js file=> pass url
+        
+        
+    }
+    console.log("After");
+}
+
+// matches => process => print => leaderboard
